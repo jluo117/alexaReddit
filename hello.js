21acf3011b2cb9f528fd4f13c8a04bd35fcf0ead
james luo <luojames52@gmail.com>
2018-10-06 16:09:57 -0700

    fist push

---
+++ hello.js
@@ -0,0 +1,10 @@
+function Get(yourUrl){
+    var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+    var Httpreq = new XMLHttpRequest(); // a new request
+    Httpreq.open("GET",yourUrl,false);
+    Httpreq.send(null);
+    return Httpreq.responseText;          
+}
+var json_obj = JSON.parse(Get("https://www.reddit.com/r/gaming/.json"));
+
+console.log(json_obj["data"]["children"][0]["data"]["title"]);
